be3b80f6c354c8e3313a80ff61f8fdd93121ef9b
mgacko <mgacko@gmail.com>
2021-11-22 17:13:02 +0100

    requirements

--- config/settings/base.py
+++ config/settings/base.py
@@ -31,7 +31,6 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # "django.contrib.humanize", # Handy template tags
-    # "grappelli",
     "django.contrib.admin",
 ]
 

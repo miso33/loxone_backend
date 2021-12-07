9b54aa09887fe641bac6d5e29faa87f353f00a6e
mgacko <mgacko@gmail.com>
2021-12-07 22:24:16 +0100

    mimet

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,6 +4,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = False
 
+mimetypes.add_type("text/css", ".css", True)
 
 sentry_sdk.init(
     dsn="https://a5797ef14d444ec7ba5336b507a778f8@o1085724.ingest.sentry.io/6097334",

0b909a03ee65d8db1f551ba12f3940231189eabc
mgacko <mgacko@gmail.com>
2021-12-07 20:39:44 +0100

    stage.py

--- config/settings/stage.py
+++ config/settings/stage.py
@@ -1,13 +1,3 @@
 from .celery import *
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = True
-
-
-sentry_sdk.init(
-    dsn="https://0e373a22cb4446f1b9b7c65033467e5b@o1070615.ingest.sentry.io/6066737",
-    integrations=[DjangoIntegration()],
-    traces_sample_rate=1.0,
-    send_default_pii=True,
-)
